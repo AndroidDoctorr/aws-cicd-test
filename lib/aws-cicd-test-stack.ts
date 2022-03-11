eb848ca545946cdb12341133bf7cc4f46bd476de
AndroidDoctorr <gbartthenerd@gmail.com>
2022-03-10 22:26:04 -0500

    added test stack for gh pipeline

--- lib/aws-cicd-test-stack.ts
+++ lib/aws-cicd-test-stack.ts
@@ -1,16 +1,25 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import {CodePipeline, CodePipelineSource, ShellStep} from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+// import { MyPipelineAppStage } from './stage';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class AwsCicdTestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsCicdTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // const pipeline = new CodePipeline(this, 'Pipeline', {
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'TestPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('AndroidDoctorr/aws-cicd-test', 'main'),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npm cdk synth'
+        ]
+      }),
+    });
   }
 }
